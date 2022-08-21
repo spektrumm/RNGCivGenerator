79101f8ce7c066b8df980538f74667c5c3290247
nate hare <51189697+spektrumm@users.noreply.github.com>
2022-08-21 05:33:23 -0700

    Add an outputted json file with the results

--- functions/dataProcessing.py
+++ functions/dataProcessing.py
@@ -1,6 +1,7 @@
 
 
 import random
+import json
 
 # get user input to select number of civs to generate
 
@@ -8,33 +9,33 @@ import random
 def getPlayers(civsList):
     usedValues = []
     playerList = []
+    outDict = {}
     numPlayers = int(input("How many players are there?\n"))
     for i in range(numPlayers):
         player = input("Enter the name of each player one at a time.\n")
         playerList.append(player)
 
-    for people in range(numPlayers):
-        # print(f'this is the length of the dict at line 52 {len(civsList)}') debug
+    with open("selectedCivs.json", 'w', encoding='utf-8') as outJson:
 
-        value = random.randint(0, len(civsList))
-        # print(value) debug
-        if value in usedValues:
-            print("Caught a duplicate, trying again.")
-            newValue = random.randint(0, len(civsList))
-            usedValues.append(newValue)
-            try:
-                civ = civsList[str(newValue)]
-            except KeyError:
-                print(KeyError)
-                civ = civsList.get(str(newValue))
-            print(f'{playerList[people]} is civ: {civ}')
-        else:
-            usedValues.append(value)
-            # print(value) debug
-            try:
-                civ = civsList[str(value)]
-            except KeyError:
-                print(KeyError)
-                civ = civsList.get(str(value))
-            # print(civ) debug
-            print(f'{playerList[people]} is civ: {civ}')
+        for people in range(numPlayers):
+
+            value = random.randint(0, len(civsList))
+            if value in usedValues:
+                print("Caught a duplicate, new number will be generated")
+                newValue = random.randint(0, len(civsList))
+                usedValues.append(newValue)
+                try:
+                    newKey = {playerList[people]: civsList[str(newValue)]}
+                    outDict.update(newKey)
+                except KeyError:
+                    print(KeyError)
+            else:
+                usedValues.append(value)
+                try:
+                    newKey = {playerList[people]: civsList[str(value)]}
+                    outDict.update(newKey)
+                except KeyError:
+                    print(KeyError)
+
+        json.dump(outDict, outJson, ensure_ascii=False, indent=4)
+        print("selectedCivs.json has been saved.")
